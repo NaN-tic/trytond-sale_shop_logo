d2e05b6f7e2153d649c30b8024f6031a2cd09ab2
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:42:34 +0100

    Cleanup tests

--- tests/test_sale_shop_logo.py
+++ tests/test_sale_shop_logo.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class SaleShopLogoTestCase(unittest.TestCase):
-    '''
-    Test Sale Shop Logo module.
-    '''
+    'Test Sale Shop Logo module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_shop_logo')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_shop_logo')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
