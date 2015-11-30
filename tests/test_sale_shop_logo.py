a9ec8b93b4aff5b66e290f7fdb7314f5c4c78142
resteve <resteve@zikzakmedia.com>
2015-12-01 00:22:45 +0100

    Tests

--- tests/test_sale_shop_logo.py
+++ tests/test_sale_shop_logo.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_shop_logo module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleShopLogoTestCase(unittest.TestCase):
+class SaleShopLogoTestCase(ModuleTestCase):
     'Test Sale Shop Logo module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_shop_logo')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_shop_logo')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_shop_logo'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleShopLogoTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
